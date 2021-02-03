606c7e8fe48369d4b382d70a15283898fb020cfa
rdvhoorn <38728066+rdvhoorn@users.noreply.github.com>
2021-02-03 11:15:59 +0100

    some styling

--- src/components/Documentation.vue
+++ src/components/Documentation.vue
@@ -1,8 +1,10 @@
 <template>
-  <h1>How to get started</h1>
-  <p>In the tab for phonemes, you can do everything related to phonemes! This includes viewing all the phonemes that are
-    used, just training single phonemes, but also taking a test. In the tab 'Words', you can do similar stuff as in the
-    phonemes tab, but then with words. In the 'Audio' tab, you can upload an audio file for processing.</p>
+  <div style="width: min(800px, 100%)">
+    <h1>How to get started</h1>
+    <p>In the tab for phonemes, you can do everything related to phonemes! This includes viewing all the phonemes that
+      are used, just training single phonemes, but also taking a test. In the tab 'Words', you can do similar stuff as
+      in the phonemes tab, but then with words. In the 'Audio' tab, you can upload an audio file for processing.</p>
+  </div>
 </template>
 
 <script lang="ts">
