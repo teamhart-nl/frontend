5721af9f39aa142f01562d858b51cec47a68912e
rdvhoorn <38728066+rdvhoorn@users.noreply.github.com>
2021-02-07 18:30:20 +0100

    lots of progress (but slow)

--- src/components/Phonemes.vue
+++ src/components/Phonemes.vue
@@ -1,26 +1,59 @@
-<template>
+<template class="temp">
   <h1>Phonemes!</h1>
-  <div v-for="item in phonemes" v-bind:key="item.name" class="p-field-checkbox">
-        <Checkbox :id="'checkbox_' + item.name" name="item.nam3" :value="item.name" v-model="selectedPhonemes"/>
-        <label :for="item">{{item.name}}</label>
-  </div>
+  <Panel header="Send a chosen phoneme" style="margin-bottom: 10px">
+    <p>To just try out how a single phoneme feels, and play it on the prototype, you can select a phoneme in the
+      dropdown menu and send it to the arduino by clicking the button.</p>
 
-  <Dropdown v-model="dropdownPhoneme" :options="phonemes" optionLabel="name" placeholder="Phoneme" :filter="true"/>
-  <Button @click="sendDropdownPhoneme()">Send phoneme!</Button>
+    <Dropdown v-model="dropdownPhoneme" :options="phonemes" optionLabel="name" placeholder="Phoneme" :filter="true" style="margin-right: 10px"/>
+    <Button @click="sendDropdownPhoneme()" style="padding: 1.2rem">Send phoneme!</Button>
+  </Panel>
 
+  <Panel header="Training">
+    <p>Select which phonemes you want to train on!</p>
+    <div v-for="item in phonemes" v-bind:key="item.name" class="p-field-checkbox" style="display: inline-block; margin-right: 20px">
+      <Checkbox :id="'checkbox_' + item.name" name="item.nam3" :value="item.name" v-model="selectedTrainPhonemes"/>
+      <label :for="item">{{ item.name }}</label>
+
+    </div>
+    <br>
+    <Panel header="Send random">
+    <Button @click="sendRandomPhoneme()" style="padding: 1.2rem">Send random phoneme!</Button>
+    </Panel>
+    <Panel header="Forced identification">
+      <p>By clicking the button, a phoneme will be send to the arduino, and you will get to see three buttons, and have to choose which one you felt.</p>
+      <Button @click="sendForcedIdentification()" style="padding: 1.2rem">Forced identification!</Button>
+      <div id="forcedIdentificationButtons"></div>
+    </Panel>
+  </Panel>
 </template>
 
-<script>
-import {defineComponent, ref} from "vue";
+<script lang="ts">
+import {createApp, defineComponent, ref} from "vue";
 import APIWrapper from "@/backend.api";
+import Button from "primevue/button";
+
+function getRandom(arr, n): any {
+  let result = new Array(n),
+      len = arr.length,
+      taken = new Array(len);
+  if (n > len)
+    throw new RangeError("getRandom: more elements taken than available");
+  while (n--) {
+    const x = Math.floor(Math.random() * len);
+    result[n] = arr[x in taken ? taken[x] : x];
+    taken[x] = --len in taken ? taken[len] : len;
+  }
+  return result;
+}
 
 export default defineComponent({
   name: 'Phonemes',
+  extends: Button,
 
   async setup() {
 
     const phonemeData = (await APIWrapper.getPhonemes()).phonemes;
-    const selectedPhonemes = ref([]);
+    const selectedTrainPhonemes = ref([]);
     const dropdownPhoneme = ref();
 
     function sendDropdownPhoneme() {
@@ -28,8 +61,47 @@ export default defineComponent({
         const json = {'phonemes': [dropdownPhoneme.value.name]}
         APIWrapper.sendPhonemeMicrocontroller(json);
       } else {
-        alert("Please select a phoneme to send")
+        alert("Please select a phoneme to send");
       }
+    }
+
+    function sendRandomPhoneme() {
+      if (selectedTrainPhonemes.value.length === 0) {
+        alert("Please select phonemes to train on");
+      } else {
+        const randomPhoneme = selectedTrainPhonemes.value[Math.floor(Math.random() * selectedTrainPhonemes.value.length)]
+        const json = {'phonemes': [randomPhoneme]};
+        APIWrapper.sendPhonemeMicrocontroller(json);
+      }
+    }
+
+    function sendForcedIdentification() {
+      const buttonDiv = document.getElementById("forcedIdentificationButtons")
+      buttonDiv.innerHTML = '';
+
+      if (selectedTrainPhonemes.value.length === 0) {
+        alert("Please select phonemes to train on");
+        return
+      }
+
+      const randomPhonemes = getRandom(selectedTrainPhonemes.value, Math.min(3, selectedTrainPhonemes.value.length));
+      // const playedPhoneme = getRandom(randomPhonemes, 1) // is a list
+
+      // APIWrapper.sendPhonemeMicrocontroller({'phonemes': playedPhoneme});
+
+
+      const textDiv = document.createElement('div');
+      textDiv.innerHTML = '<p>Which phoneme was just played?</p>';
+      buttonDiv.appendChild(textDiv);
+
+      randomPhonemes.forEach((phoneme) => {
+        const div = document.createElement('div');
+        div.style = "display: inline-block; margin-right: 10px";
+
+        buttonDiv.appendChild(div);
+        createApp(Button, {label: phoneme}).mount(div);
+
+      })
     }
 
     // eslint-disable-next-line no-unused-labels
@@ -40,10 +112,12 @@ export default defineComponent({
 
     return {
       phonemes,
-      selectedPhonemes,
+      selectedTrainPhonemes,
       dropdownPhoneme,
 
       sendDropdownPhoneme,
+      sendRandomPhoneme,
+      sendForcedIdentification
     }
   }
 })
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import '@babel/polyfill'
+import 'mutationobserver-shim'
 import { createApp } from 'vue'
 import App from './App.vue'
 
@@ -5,16 +7,18 @@ import PrimeVue from 'primevue/config'
 import Button from "primevue/button";
 
 import 'primevue/resources/themes/md-light-indigo/theme.css';
+// import './assets/layout/theme.css';
 import 'primevue/resources/primevue.min.css';
 import 'primeicons/primeicons.css';
-import './assets/layout/reset.css';
-import './assets/layout/style.css';
 import 'primeflex/primeflex.css';
-
 import TabMenu from "primevue/tabmenu";
 import {router} from "@/router/router";
 import Checkbox from "primevue/checkbox";
 import Dropdown from "primevue/dropdown";
+import Panel from "primevue/panel";
+
+import './assets/layout/reset.css';
+import './assets/layout/style.css';
 
 const app = createApp(App)
 
@@ -25,5 +29,6 @@ app.component('Button', Button);
 app.component('TabMenu', TabMenu);
 app.component('Checkbox', Checkbox);
 app.component('Dropdown', Dropdown);
+app.component('Panel', Panel);
 
 app.mount('#app')
